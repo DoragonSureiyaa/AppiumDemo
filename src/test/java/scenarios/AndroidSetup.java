b8deb598f4b8d0715d80932c4680469a021bc1c0
Tomasz Skrzypek <tomasz.skrzypek@lumesse.com>
2016-03-17 13:09:12 +0100

    first commit

--- src/test/java/scenarios/AndroidSetup.java
+++ src/test/java/scenarios/AndroidSetup.java
@@ -1,11 +1,11 @@
 package scenarios;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.BeforeClass;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 
 import java.io.File;
@@ -17,27 +17,26 @@ import java.net.URL;
  */
 public abstract class AndroidSetup {
 
-    private static final File APP_DIR = new File("/REPO/testdroid-samples-master/testdroid-samples-master/appium/sample-scripts/AppiumDemo/apps");
 
+    private AppiumDriverLocalService service;
+    //private static final File APP_DIR = new File("/REPO/testdroid-samples-master/testdroid-samples-master/appium/sample-scripts/AppiumDemo/apps");
+    private static final File APP_DIR = new File("C://");
     protected AppiumDriver driver;
-
     private DesiredCapabilities capabilities = new DesiredCapabilities();
     private File app;
 
-    public void setUpTest() {
-        app = new File(APP_DIR, "e249.apk");
-    }
-
     @BeforeMethod
     public void prepareAndroidForAppium() throws MalformedURLException {
-        setUpTest();
+        //startAppiumServer();
+
+        app = new File(APP_DIR, "e249.apk");
 
         capabilities.setCapability("device", "Android");
         capabilities.setCapability("deviceName", "Android6");
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("app", app.getAbsolutePath());
 
-        driver =  new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities) {
+        driver =  new AppiumDriver(new URL("http://192.168.0.7:4726/wd/hub"), capabilities) {
             @Override
             public WebElement scrollTo(String s) {
                 return null;
@@ -48,9 +47,41 @@ public abstract class AndroidSetup {
                 return null;
             }
         };
+
+        setUpTest();
     }
 
     protected AppiumDriver getDriver() {
         return driver;
+    }
+
+    protected void setUpTest() {
+    }
+
+    String osName = System.getProperty("os.name");
+    String deviceUnderExecution = "Android6";
+
+    private void startAppiumServer() {
+        service = AppiumDriverLocalService.buildDefaultService();
+
+        if (osName.contains("Mac")) {
+            service = AppiumDriverLocalService.buildService(new AppiumServiceBuilder()
+                    .usingDriverExecutable(new File("/Applications/Appium.app/Contents/Resources/node/bin/node"))
+                    .withAppiumJS(new File("/Applications/Appium.app/Contents/Resources/node_modules/appium/bin/appium.js"))
+                    .withIPAddress("127.0.0.1")
+                    .usingPort(4723)
+                    .withLogFile(new File("target/"+deviceUnderExecution+".log")));
+        }
+        else if (osName.contains("Windows")) {
+            service = AppiumDriverLocalService.buildService(new AppiumServiceBuilder()
+                    .withIPAddress("127.0.0.1")
+                    .usingPort(4723));
+        }
+        else {
+            Assert.fail("Unspecified OS found, Appium can't run");
+        }
+
+        System.out.println("- - - - - - - - Starting Appium Server- - - - - - - - ");
+        service.start();
     }
 }
